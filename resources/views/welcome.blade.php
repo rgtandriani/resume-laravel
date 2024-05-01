c3ad15cadbb81371d0fcdc4438f992b4c34093f1
regita <rgtandriani@gmail.com>
2024-05-01 19:49:05 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,345 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, maximum-scale=1">
+<title>Bootstrap Resume Template | Webthemez</title>
+<link rel="icon" href="at/favicon.png" type="image/png">
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="js/fancybox/jquery.fancybox.css" type="text/css" media="screen" />
+<link href="css/style.css" rel="stylesheet" type="text/css"> 
+<link href="css/font-awesome.css" rel="stylesheet" type="text/css"> 
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+ 
+<!--[if lt IE 9]>
+    <script src="js/respond-1.1.0.min.js"></script>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/html5element.js"></script>
+<![endif]-->
+ 
+</head>
+<body>
+
+<!--Hero_Section-->
+<section id="hero_section" class="top_cont_outer">
+  <div class="hero_wrapper">
+    <div class="container">
+      <div class="hero_section">
+        <div class="row">
+          <div class="col-md-6">
+		   
+            <div class="top_left_cont intro zoomIn wow animated"> 
+              <h2>I’m Regita Andriani<br> <strong>Fresh graduate</strong></h2>
+              <p>
+			 saya lulusan dari manajemen informatika dengan keahlian full stack developer dan ahli dalam pemprograman </p>
+                <div class="underline"></div>             
+              <ul class="social_links">
+				<li class="twitter animated bounceIn wow delay-02s animated" style="visibility: visible; animation-name: bounceIn;"><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+				<li class="facebook animated bounceIn wow delay-03s animated" style="visibility: visible; animation-name: bounceIn;"><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+				<li class="pinterest animated bounceIn wow delay-04s animated" style="visibility: visible; animation-name: bounceIn;"><a href="javascript:void(0)"><i class="fa fa-pinterest"></i></a></li>
+				<li class="gplus animated bounceIn wow delay-05s animated" style="visibility: visible; animation-name: bounceIn;"><a href="javascript:void(0)"><i class="fa fa-google-plus"></i></a></li> 
+			  </ul> 
+			  </div>
+          </div> 
+		  <div class="col-md-6">
+		   <img alt="" src="img/profilee-pic.jpg"> 
+		  </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!--Hero_Section--> 
+
+<!--Header_section-->
+<header id="header_wrapper"> 
+    <div class="header_box">
+    <!--  <div class="logo"><a href="#"><img src="img/logo.png" alt="logo"></a></div>-->
+	  <nav class="navbar navbar-inverse" role="navigation">
+      <div class="navbar-header">
+        <button type="button" id="nav-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#main-nav"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        </div>
+	    <div id="main-nav" class="collapse navbar-collapse navStyle">
+			<ul class="nav navbar-nav" id="mainNav">
+			  <li class="active"><a href="#hero_section" class="scroll-link">Home</a></li>
+			  <li><a href="#aboutUs" class="scroll-link">About Me</a></li>
+			  <li><a href="#service" class="scroll-link">Skills</a></li>
+			  <li><a href="#experience" class="scroll-link">Experience</a></li>
+			 
+			  <li><a href="#contact" class="scroll-link">Contact</a></li>
+			</ul>
+      </div>
+	 </nav>
+    </div> 
+</header>
+<!--Header_section--> 
+
+<section id="aboutUs"><!--Aboutus-->
+<div class="inner_wrapper aboutUs-container fadeInLeft animated wow">
+  <div class="container">
+    <h2>About Me</h2>
+	<h6>bersemangat tinggi, senang belajar hal baru, cepat belajar</h6>
+    <div class="inner_section"> 
+	  <div class="row">
+						<div class="col-lg-12 about-us">
+							<div class="row">
+								<div class="col-md-6">
+									<h3>saya development web</h3>
+									<p>
+										 mempelajari banyak hal akan pemprograman dan manajemen 
+									</p>
+									<p>
+										 banyak memepelajari akuntansi dan perhitungan menggunakan sistem operasi lain seperti excel.berikut yang sedang dipelajari
+									</p>
+									<ul class="about-us-list">
+										<li class="points">HTML </li>
+										<li class="points">fRAMEWORK ci3,LARAVEL DAN VUEJS </li>
+										<li class="points">fullstack dev </li>
+                                        <li class="points">sistem excel </li>
+										<li class="points">API</li>
+									</ul><!-- /.about-us-list -->
+								 
+								</div><!-- /.col-md-6 -->
+								
+							<div class="col-md-6"> <br/>
+							<p>
+										berikut persentasenya.
+									</p><br/><br/>
+							        <div class="barWrapper">
+									 <span class="progressText"><B>HTML5</B></span>
+									<div class="progress">
+									  <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" >   
+											<span  class="popOver" data-toggle="tooltip" data-placement="top" title="85%"> </span>     
+									</div>
+									</div>
+
+									<div class="barWrapper">
+									 <span class="progressText"><B>CSS3</B></span>
+									<div class="progress ">
+									  <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="10" aria-valuemax="100" style="">
+										 <span  class="popOver" data-toggle="tooltip" data-placement="top" title="75%"> </span>  
+									  </div>
+									  
+									</div>
+									</div>
+
+									<div class="barWrapper">
+									 <span class="progressText"><B>BOOTSRAPT</B></span>
+									<div class="progress">
+									  <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100">
+										 <span  class="popOver" data-toggle="tooltip" data-placement="top" title="65%"> </span>  
+									  </div>
+									</div>
+									</div>
+									<div class="barWrapper">
+									 <span class="progressText"><B>JQUERY</B></span>
+									<div class="progress">
+									  <div class="progress-bar" role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100">
+											 <span  class="popOver" data-toggle="tooltip" data-placement="top" title="55%"> </span>  
+									  </div>
+									</div>
+									</div> 
+
+									</div>
+							</div><!-- /.col-md-6 -->
+							</div><!-- /.row -->	
+						</div><!-- /.col-lg-12 -->
+					</div>
+      
+    </div>
+  </div> 
+  </div>
+</section>
+<!--Aboutus--> 
+
+
+<!--Service-->
+<section  id="service">
+  <div class="container">
+    <h2>Skills</h2>
+	<h6>Pemprograman, excel, bahasa korea</h6>
+    <div class="service_wrapper">
+      <div class="row">
+        <div class="col-md-3">
+		<div class="service_icon delay-03s animated wow  zoomIn"> <span><i class="fa fa-android"></i></span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">excel</h3>
+            <p class="animated fadeInDown wow"> </p>
+          </div>
+        </div>
+        <div class="col-md-3"> 
+		<div class="service_icon icon2  delay-03s animated wow zoomIn"> <span><i class="fa fa-apple"></i></span> </div> 
+		<div class="service_block">
+            <h3 class="animated fadeInUp wow">bahasa korea</h3>
+            <p class="animated fadeInDown wow">  </p>
+          </div>
+        </div>
+        <div class="col-md-3">
+		<div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-html5"></i></span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">Manajamen</h3>
+            <p class="animated fadeInDown wow">  </p>
+          </div>
+        </div>
+		  <div class="col-md-3">
+		<div class="service_icon icon3  delay-03s animated wow zoomIn"> <span><i class="fa fa-user"></i></span> </div>
+          <div class="service_block">
+            
+            <h3 class="animated fadeInUp wow">Development</h3>
+            <p class="animated fadeInDown wow">  </p>
+          </div>
+        </div>
+      </div> 
+    </div>
+  </div>
+</section>
+<!--Service-->
+
+
+ 
+
+<section id="experience" class="timeline">
+<div class="container">
+	 <h2>Experience</h2>
+	<h6></h6>
+    <div class="qa-message-list" id="wallmessages">
+    				<div class="message-item" id="m16">
+						<div class="message-inner">
+							<div class="message-head clearfix"> 
+								<div class="user-detail">
+									<h5 class="handle">Hodja Collection</h5>
+									<div class="post-meta">
+										<div class="asker-meta">
+											<span class="qa-message-what"></span>
+											<span class="qa-message-when">
+												<span class="qa-message-when-data">3 nov</span>
+											</span> 
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="qa-message-content">
+								Admin Produksi
+							</div>
+					</div></div>
+					
+					<div class="message-item" id="m9">
+						<div class="message-inner">
+							<div class="message-head clearfix"> 
+								<div class="user-detail">
+									<h5 class="handle">PT. Mattel Indonesia</h5>
+									<div class="post-meta">
+										<div class="asker-meta">
+											<span class="qa-message-what"></span>
+											<span class="qa-message-when">
+												<span class="qa-message-when-data">2021-2024</span>
+											</span> 
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="qa-message-content">
+								IT Finance
+							</div>
+					</div></div>
+					
+					
+					</div></div>
+					
+				</div>
+</div>
+
+</section>
+<!--/Team-->
+<!--Footer-->
+<footer class="footer_wrapper" id="contact">
+  <div class="container">
+    <section class="page_section contact" id="contact">
+      <div class="contact_section">
+        <h2>Get In Touch</h2>
+	<h6></h6>
+     
+      </div>
+      <div class="row">
+        <div class="col-lg-4 wow fadeInLeft">	
+		 <div class="contact_info">
+                            <div class="detail">
+                                <h4>Regita Andriani</h4>
+                                <p>PURWAKARTA</p>
+                            </div>
+                            <div class="detail">
+                                <h4>call us</h4>
+                                <p>089506822544</p>
+                            </div>
+                            <div class="detail">
+                                <h4>Email us</h4>
+                                <p>RGTANDRIANI@GMAIL.COM</p>
+                            </div> 
+                        </div>
+       		  
+			  
+          
+          <ul class="social_links">
+            <li class="twitter animated bounceIn wow delay-02s"><a href="javascript:void(0)"><i class="fa fa-twitter"></i></a></li>
+            <li class="facebook animated bounceIn wow delay-03s"><a href="javascript:void(0)"><i class="fa fa-facebook"></i></a></li>
+            <li class="pinterest animated bounceIn wow delay-04s"><a href="javascript:void(0)"><i class="fa fa-pinterest"></i></a></li>
+            <li class="gplus animated bounceIn wow delay-05s"><a href="javascript:void(0)"><i class="fa fa-google-plus"></i></a></li> 
+          </ul>
+        </div>
+        <div class="col-lg-8 wow fadeInLeft delay-06s">
+         	  <!--NOTE: Update your email Id in "contact_me.php" file in order to receive emails from your contact form-->
+        
+		<form name="sentMessage" id="contactForm"  novalidate> 
+		 <div class="control-group">
+                    <div class="controls">
+			<input type="text" class="form-control" 
+			   	   placeholder="Full Name" id="name" required
+			           data-validation-required-message="Please enter your name" />
+			  <p class="help-block"></p>
+		   </div>
+	         </div> 	
+                <div class="control-group">
+                  <div class="controls">
+			<input type="email" class="form-control" placeholder="Email" 
+			   	            id="email" required
+			   		   data-validation-required-message="Please enter your email" />
+		</div>
+	    </div> 	
+			  
+               <div class="control-group">
+                 <div class="controls">
+				 <textarea rows="10" cols="100" class="form-control" 
+                       placeholder="Message" id="message" required
+		       data-validation-required-message="Please enter your message" minlength="5" 
+                       data-validation-minlength-message="Min 5 characters" 
+                        maxlength="999" style="resize:none"></textarea>
+		</div>
+               </div> 		 
+	     <div id="success"> </div> <!-- For success/fail messages -->
+	    <button type="submit" class="btn btn-primary pull-right">Send</button><br />
+          </form>
+        </div>
+      </div>
+    </section>
+  </div>
+  <div class="container">
+    <div class="footer_bottom"><span>Copyright regita. <a href="https://webthemez.com/free-bootstrap-templates/" target="_blank"></a> </span> </div>
+  </div>
+</footer>
+
+<script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/jquery-scrolltofixed.js"></script>
+<script type="text/javascript" src="js/jquery.nav.js"></script> 
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.isotope.js"></script>
+<script src="js/fancybox/jquery.fancybox.pack.js" type="text/javascript"></script> 
+<script type="text/javascript" src="js/wow.js"></script> 
+ <script src="contact/jqBootstrapValidation.js"></script>
+ <script src="contact/contact_me.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+
+</body>
+</html>
